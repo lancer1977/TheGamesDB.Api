9bd242d91247cf7f6a5036664f745861e94fe74f
chris <lancer1977@gmail.com>
2024-06-22 12:43:48 -0400

    gamesdb

--- GamesDB.RestAsync/Model/BaseApiResponse.cs
+++ GamesDB.RestAsync/Model/BaseApiResponse.cs
@@ -6,6 +6,7 @@ namespace GamesDB.RestAsync.Model
         public string Status { get; set; }
         [JsonPropertyName("remaining_monthly_allowance")]
         public int RemainingMonthlyAllowance { get; set; }
+        [JsonPropertyName("extra_allowance")]
         public int ExtraAllowance { get; set; }
     }
 }
